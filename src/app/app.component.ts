10f74da723803a8b948a9f72bdf92cf969b77f49
Andero <64332987+Nifilia@users.noreply.github.com>
2025-07-10 14:35:00 +0000

    Fonts, Styles & Footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, NavbarComponent],
+  imports: [HomeComponent, NavbarComponent, FooterComponent],
   template: `
     <app-navbar />
     <app-home />
+    <app-footer />
   `,
   styles: [],
 })
